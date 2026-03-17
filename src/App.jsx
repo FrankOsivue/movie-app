46f645a7f4c5ab0291e9d862f34082fa0bd2a275
Mark Francis <markfranksive@gmail.com>
2026-03-17 14:13:53 +0100

    Add Taiwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { useState, useEffect } from 'react';
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`You have ${hasLiked ? 'liked' : 'unliked'} ${title}`);
-  }, [hasLiked]);
-  return (
-    <div
-      className='card'
-      onClick={() => setCount(count + 1)}
-    >
-      <h2>{title}</h2>
-      <button
-        onClick={() => {
-          setHasLiked(!hasLiked);
-        }}
-      >
-        {hasLiked ? '♥' : '♡'} - {count}
-      </button>
-    </div>
-  );
-};
+import React from 'react';
 
 const App = () => {
   return (
-    <div className='card-container'>
-      <Card title='The Matrix' />
-      <Card title='Inception' />
-      <Card title='Interstellar' />
+    <div>
+      <h1 class='text-3xl font-bold underline'>Hello world!</h1>
     </div>
   );
 };
+
 export default App;
