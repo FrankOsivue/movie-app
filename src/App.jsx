fa80c6a498d5dfc7d1233c322a0f2f32bfe34c09
Mark Francis <markfranksive@gmail.com>
2026-03-18 13:06:54 +0100

    Add hero section and project assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Search from './components/Search';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState('batman');
   return (
-    <div>
-      <h1 class='text-3xl font-bold underline'>Hello world!</h1>
-    </div>
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        <header>
+          <h1>
+            <img
+              src='./hero.png'
+              alt='hero banner'
+            />
+            Find <span className='text-gradient'>Movies</span> you will enjoy
+            without the hassle
+          </h1>
+        </header>
+        <Search
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
+      </div>
+    </main>
   );
 };
 
